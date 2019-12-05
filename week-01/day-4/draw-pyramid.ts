a7e2c2a48bd6defab52598a743790d40fc54a441
Noemi Puskas <amifontos@gmail.com>
2019-12-05 16:22:31 +0100

    pyramid

--- week-01/day-4/draw-pyramid.ts
+++ week-01/day-4/draw-pyramid.ts
@@ -4,15 +4,30 @@
 let lineCount2: number = 4;
 
 for (let i = 1; i <= lineCount2; i++) {
-    
-    let spaces: string = '';
-    let stars: string = '';
 
-    for (let k = lineCount2-1; k > 0; k--) {
+    let spaces: string = '';
+    let stars: string = '*';
+
+    for (let m = 1; m <= lineCount2 - i; m++) {
         spaces = spaces + ' ';
     }
-    for (let j = 1; j <= i; j++) {
-        stars = stars + '*';
+
+    if (i >= 2) {
+        for (let j = 2; j <= i; j++) {
+            stars = stars + '**';
+        }
     }
+
     console.log(spaces + stars);
 }
+
+
+// Write a program that draws a
+// pyramid like this:
+//
+//   *
+//  ***
+// *****
+//*******
+//
+// The pyramid should have as many lines as lineCount is
