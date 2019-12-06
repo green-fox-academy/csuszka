4da385e960c86cbafeed92f3aa9054416034e133
Noemi Puskas <amifontos@gmail.com>
2019-12-06 15:54:01 +0100

    style correction for diamonds (TypeScript styleguide)

--- week-01/day-4/draw-diamond.ts
+++ week-01/day-4/draw-diamond.ts
@@ -1,77 +1,60 @@
-
 'use strict';
 
-let lineCount3: number = 8;
+const lineCount3: number = 12;
 let changesDirectionAfter: number;
 
-
 if (lineCount3 % 2 > 0) {
-    changesDirectionAfter = lineCount3 / 2 + 0.5;
+  changesDirectionAfter = lineCount3 / 2 + 0.5;
 }
 else {
-    changesDirectionAfter = lineCount3 / 2;
+  changesDirectionAfter = lineCount3 / 2;
 }
-
-console.log('The diamond changes direction after the line' + changesDirectionAfter);
-
-/* upper half*/
-
+/* the loop that goes through the whole diamond */
 for (let i = 1; i <= lineCount3; i++) {
 
-    let spaces: string = '';
-    let stars: string = '*';
+  let spaces: string = '';
+  let stars: string = '*';
+  /* upper half*/
+  if (i <= changesDirectionAfter) {
 
-
-    if (i <= changesDirectionAfter) {
-
-        for (let m = 1; m <= changesDirectionAfter - i; m++) {
-            spaces = spaces + ' ';
-        }
-
-        if (i >= 2) {
-            for (let j = 2; j <= i; j++) {
-                stars = stars + '**';
-            }
-        }
+    for (let m = 1; m <= changesDirectionAfter - i; m++) {
+      spaces = spaces + ' ';
     }
+    if (i >= 2) {
+      for (let j = 2; j <= i; j++) {
+        stars = stars + '**';
+      }
+    }
+  }
+  /*lower half*/
+  else {
 
-    /*lower half*/
+    let spacelimit: number;
 
+    if (lineCount3 % 2 > 0) {
+      spacelimit = i - changesDirectionAfter;
+    }
     else {
-
-        let spacelimit: number;
-        if (lineCount3 % 2 > 0) {
-            spacelimit = i - changesDirectionAfter;
-        }
-        else {
-            spacelimit = i - changesDirectionAfter - 1;
-        }
-
-        for (let t = 1; t <= spacelimit; t++) {
-            spaces = spaces + ' ';
-        }
-        if (i > changesDirectionAfter) {
-            for (let w = 1; w <= lineCount3 - i; w++) {
-                stars = stars + '**';
-            }
-        }
+      spacelimit = i - changesDirectionAfter - 1;
     }
-
-
-    console.log(spaces + stars);
+    for (let t = 1; t <= spacelimit; t++) {
+      spaces = spaces + ' ';
+    }
+    if (i > changesDirectionAfter) {
+      for (let w = 1; w <= lineCount3 - i; w++) {
+        stars = stars + '**';
+      }
+    }
+  }
+  console.log(spaces + stars);
 }
 
-
-
-// Write a program that draws a
-// diamond like this:
+// draws a diamond like this:
 //
-//   * 1
-//  *** 3
-// ***** 5 
-//******* 7
-// ***** 5
-//  *** 3 
-//   * 1
-//
-// The diamond should have as many lines as lineCount is
+// 3   * 1
+// 2  *** 3
+// 1 ***** 5 
+// 0******* 7
+// 1 ***** 5
+// 2  *** 3 
+// 3   * 1
