4ccf5b8a3a19371181374e70996140450920593b
Noemi Puskas <amifontos@gmail.com>
2019-12-09 17:30:33 +0100

    bubble

--- week-02/day-1/bubble.ts
+++ week-02/day-1/bubble.ts
@@ -3,10 +3,36 @@
 //  Returns a list where the elements are sorted in ascending numerical order
 //  Make a second boolean parameter, if it's `true` sort that list descending
 
-//  Example:
-console.log(bubble([34, 12, 24, 9, 5]));
-//  should print [5, 9, 12, 24, 34]
-console.log(advancedBubble([34, 12, 24, 9, 5], true));
-//  should print [34, 24, 12, 9, 5]
+// arr [1] => arr[0]
+
 
 'use.strict';
+
+function bubble(arr: number[], beDescending?: boolean): number[] {
+  for (let a: number = 0; a < arr.length; a++) {
+    /*going through the array once*/
+    for (let i: number = 0; i < arr.length; i++) {
+      if (arr[i + 1] < arr[i]) {
+        let temporary: number = arr[i + 1];
+        arr[i + 1] = arr[i];
+        arr[i] = temporary;
+      }
+    }
+  }
+  if (beDescending === false) {
+    return arr;
+  }
+  else {
+    let descending: number[] = [];
+    for (let i = arr.length - 1; i >= 0; i--) {
+      descending.push(arr[i]);
+    }
+    return descending;
+  }
+}
+
+//  Tests:
+console.log(bubble([34, 12, 24, 9, 5]));
+//  should print [5, 9, 12, 24, 34]
+console.log(bubble([34, 12, 24, 9, 5], true));
+//  should print [34, 24, 12, 9, 5]
--- week-02/day-1/subint.ts
+++ week-02/day-1/subint.ts
@@ -3,18 +3,23 @@
 //  Returns the indices of the numbers in the array of which the first number is a part of
 //  Or returns an empty list if the number is not part of any of the numbers in the array
 
-// Example
+function subint(needle: number, haystack: number[]): number[] {
+  let result: number[] = [];
+  /* going through the input array*/
+  for (let i: number = 0; i < haystack.length; i++) {
+  /*evaluation of given array element*/
+  let divideThis: number = haystack[i];
+    for (let divideThis = haystack[i]; divideThis > 1; divideThis = divideThis) {
+      if (divideThis % 10 === needle || divideThis / 10 === needle){
+        result.push[i];
+    }
+    divideThis = divideThis/10;
+  }
+  return result;
+}
+
+// Test
 console.log(subint(1, [1, 11, 34, 52, 61]));
 // should print: `[0, 1, 4]`
 console.log(subint(9, [1, 11, 34, 52, 61]));
 // should print: '[]'
-
-function subint(lookingFor: number, hayStack: number[]): number[] {
-  let result = [];
-  for (let i: number = 0; i < hayStack.length; i++) {
-    if (lookingFor === hayStack[i]) {
-      result.push[i];
-    }
-  }
-  return result;
-}
