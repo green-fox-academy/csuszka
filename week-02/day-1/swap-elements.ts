f56b5de90acaa62021c65680980915f3b9040ba0
Noemi Puskas <amifontos@gmail.com>
2019-12-09 14:25:21 +0100

    swap elements

---
+++ week-02/day-1/swap-elements.ts
@@ -0,0 +1,7 @@
+// -  Create a variable named `abc` with the following content: `["Arthur", "Boe", "Chloe"]`
+// -  Swap the first and the third element of `abc`
+
+let abc: string[] = ["Arthur", "Boe", "Chloe"];
+abc = [abc[2], abc[1], abc[0]];
+
+console.log(abc);
