9fc7820deba070e44e4e2dc6944596cf18fa21a6
Noemi Puskas <amifontos@gmail.com>
2019-12-09 15:01:21 +0100

    append-a

---
+++ week-02/day-1/append-a.ts
@@ -0,0 +1,13 @@
+// -  Create an array variable named `animals`
+//    with the following content: `["koal", "pand", "zebr"]`
+// -  Add all elements an `"a"` at the end
+
+'use strict'
+
+let animals: string [] = ["koal", "pand", "zebr"];
+
+let fullAnimals = animals.map(function(x){
+    return x + 'a';
+});
+
+console.log(fullAnimals);
