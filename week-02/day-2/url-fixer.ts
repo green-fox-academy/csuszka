85e6679ad42e00eddbca71269c26a5d61e0e93b0
Noemi Puskas <amifontos@gmail.com>
2019-12-10 12:28:09 +0100

    url-fixer

---
+++ week-02/day-2/url-fixer.ts
@@ -0,0 +1,7 @@
+'use strict';
+// Accidentally I got the wrong URL for a funny subreddit. It's probably "odds" and not "bots"
+// Also, the URL is missing a crutial component, find out what it is and insert it too!
+
+let url: string = 'https//www.reddit.com/r/nevertellmethebots';
+let newUrl: string = url.replace('https', 'https:').replace('bots','odds');
+console.log(newUrl);
