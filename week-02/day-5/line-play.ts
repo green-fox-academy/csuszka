0bc5602044779e777d4e0ebbbdc308c4db7846e3
Noemi Puskas <amifontos@gmail.com>
2019-12-13 13:36:31 +0100

    working line-play'

--- week-02/day-5/line-play.ts
+++ week-02/day-5/line-play.ts
@@ -7,25 +7,21 @@ const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
 // drawing the net
 
 function drawNet(distance: number, color1: string, color2: string, canvasSize): void {
-
   //green lines
   ctx.beginPath();
   ctx.strokeStyle = color1;
   for (let i: number = 0; i * distance < canvasSize; i++) {
-    console.log('asd');
     ctx.moveTo(0, i * distance);
     ctx.lineTo(distance * (i + 1), canvasSize);
     ctx.stroke();
   }
   //purple lines
+  ctx.beginPath();
   ctx.strokeStyle = color2;
   for (let j: number = 0; j * distance < canvasSize; j++) {
-    ctx.moveTo(distance, j * distance);
-    ctx.lineTo(distance - ((j + 1) * distance), canvasSize);
+    ctx.moveTo(canvasSize, (j + 1) * distance);
+    ctx.lineTo(j * distance, 0);
     ctx.stroke();
   }
-
 }
-
-
 drawNet(100, 'green', 'purple', 500);
