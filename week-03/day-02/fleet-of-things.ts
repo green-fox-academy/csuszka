59136c52db419d5271a07d6664d309998d47fe67
Noemi Puskas <amifontos@gmail.com>
2019-12-17 14:38:11 +0100

    fleet-of-things todo app creation

--- week-03/day-02/fleet-of-things.ts
+++ week-03/day-02/fleet-of-things.ts
@@ -1,8 +1,43 @@
-Learn more or give us feedback
+'use strict';
+
 import { Thing } from './thing';
 import { Fleet } from './fleet';
 
-let fleet = new Fleet();
+
+
+class FleetOfThings {
+  fleet: Fleet;
+
+  // consturctor{
+  //   this.fleet = new Fleet();
+  // }
+
+  main() {
+    this.fleet = new Fleet();
+  }
+
+  print() {
+    let thingsArray = this.fleet.getThings();
+    for (let i: number = 0; i < thingsArray.length; i++) {
+      if (thingsArray[i].getCompleted() == true) {
+        console.log('[x] ' + thingsArray[i].getName());
+      } else {
+        console.log('[ ] ' + thingsArray[i].getName());
+      }
+    }
+  }
+}
+
+let fleetOfToDo = new FleetOfThings();
+fleetOfToDo.main();
+fleetOfToDo.fleet.add(new Thing('Get milk'));
+fleetOfToDo.fleet.add(new Thing('Remove the obstacles'));
+fleetOfToDo.fleet.add(new Thing('Stand up'));
+fleetOfToDo.fleet.getThings()[2].complete();
+fleetOfToDo.fleet.add(new Thing('Eat lunch'));
+fleetOfToDo.fleet.getThings()[3].complete();
+fleetOfToDo.print();
+
 
 // -  You have the `Thing` class
 // -  You have the `Fleet` class
