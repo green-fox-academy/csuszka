a5cd2bcdbf94849ba58d1a38db84b63c69c845c2
Noemi Puskas <amifontos@gmail.com>
2019-12-18 16:32:51 +0100

    initializing Pirate class

---
+++ week-03/day-02/Pirate.ts
@@ -0,0 +1,65 @@
+'use strict';
+
+class Pirate {
+  rumCounter: number = 0;
+  parrotCounter: number = 0;
+  alive: boolean = true;
+
+  drinksomeRum() {
+    this.rumCounter++;
+  }
+  
+  howsItGoingMate() {
+    if (this.alive) {
+      if (0 <= this.rumCounter && this.rumCounter <= 4) {
+        console.log('Pour me anudder!')
+      } else {
+        console.log('Arghh, I\'ma Pirate. How d\'ya d\'ink its goin?');
+      }
+    } else {
+      console.log('He\'s dead :(');
+    }
+  }
+
+  die() {
+    this.alive = false;
+  }
+
+  brawl(matey: Pirate) {
+    if (this.alive) {
+      {
+        if (matey.alive) {
+          let fate: number = Math.random();
+          if (fate <= 1 / 3) {
+            matey.die();
+          }
+          else if (1 / 3 < fate && fate <= 2 / 3) {
+            this.die();
+          } else {
+            console.log('Oh. They both passed out.')
+          }
+        } else {
+          console.log('No time to waste on this one, he\'s dead');
+        }
+      }
+    } else {
+      console.log('He\'s dead :(');
+    }
+  }
+}
+
+// Create a Pirate class. While you can add other fields and methods, you must have these methods:
+
+// drinkSomeRum() - intoxicates the Pirate some
+// howsItGoingMate() - when called, the Pirate replies, if drinkSomeRun was called:-
+// 0 to 4 times, "Pour me anudder!"
+// else, "Arghh, I'ma Pirate. How d'ya d'ink its goin?", the pirate passes out and sleeps it off.
+// If you manage to get this far, then you can try to do the following.
+
+// die() - this kills off the pirate, in which case, DrinkSomeRum, etc. just result in he's dead.
+// brawl(x) - where pirate fights another pirate (if that other pirate is alive) and there's a 1/3 chance, 1 dies, the other dies or they both pass out.
+// And... if you get that far...
+
+// Add a parrot.
+
+export { Pirate }
