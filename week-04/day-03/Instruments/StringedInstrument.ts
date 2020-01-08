1382b4544e908e5ef5160b914959677aea539b6d
Noemi Puskas <amifontos@gmail.com>
2020-01-08 13:49:54 +0100

    instruments working

---
+++ week-04/day-03/Instruments/StringedInstrument.ts
@@ -0,0 +1,18 @@
+'use strict';
+
+import { Instrument } from './Instrument';
+
+export abstract class StringedInstrument extends Instrument {
+  protected numberOfStrings: number;
+  constructor(name: string, numberOfStrings: number = 6) {
+    super(name);
+    this.numberOfStrings = numberOfStrings;
+  }
+
+  abstract sound(): string;
+
+  play(): void {
+    console.log(`${this.name}, a ${this.numberOfStrings}-stringed instrument that goes ${this.sound()}`);
+  }
+
+}
