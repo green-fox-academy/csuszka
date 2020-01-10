d005bf311d18a806690b170dee671d9453d2fac1
Noemi Puskas <amifontos@gmail.com>
2020-01-10 09:10:41 +0100

    buggy test case corrected

--- week-04/day-04/CowsAndBulls-test.ts
+++ week-04/day-04/CowsAndBulls-test.ts
@@ -3,24 +3,26 @@
 import test from 'tape';
 import {CowsAndBulls} from './CowsAndBulls'
 
+let bocika = new CowsAndBulls();
+
 test('constructor variable initilaization', function (t){
-t.equal((new CowsAndBulls).numberOfGuesses, 0 );
-t.ok((new CowsAndBulls).generatedNumber);
-t.equal((new CowsAndBulls).generatedNumber / 1000 > 1 && 10000 > (new CowsAndBulls).generatedNumber , true);
-t.ok((new CowsAndBulls).inGame);
-t.equal((new CowsAndBulls).generatedArray.length, 4 );
+t.equal(bocika.numberOfGuesses, 0 );
+t.ok(bocika.generatedNumber);
+t.equal(bocika.generatedNumber / 1000 > 1 && 10000 > bocika.generatedNumber , true);
+t.ok(bocika.inGame);
+console.log(bocika.generatedArray.length);
+t.equal(bocika.generatedArray.length, 4 );
 t.end();
 })
 
 test('state method initializes with true', function (t){
-  t.ok((new CowsAndBulls).getState());
+  t.ok(bocika.getState());
   t.end();
   })
 
   test('guess method returns numbers less than 5', function (t){
-    let boci = new CowsAndBulls();
-    boci.guess(1984);
-    t.equal(boci.getCows() < 5, true );
-    t.equal(boci.getBulls() < 5, true );
+    bocika.guess(1984);
+    t.equal(bocika.getCows() < 5, true );
+    t.equal(bocika.getBulls() < 5, true );
     t.end();
     })
