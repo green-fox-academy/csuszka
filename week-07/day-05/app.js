7dc8ac0876b32a34e9db2f772c881cf62d47baa5
Noemi Puskas <amifontos@gmail.com>
2020-02-03 14:47:52 +0100

    reddit posts endpoint get and post works

--- week-07/day-05/app.js
+++ week-07/day-05/app.js
@@ -18,8 +18,7 @@ conn.connect((err) => {
     console.error('Cannot connect to the database', err);
     return;
   }
-  console.log('Connection established');
-  conn.end();
+  console.log('Connection established'); //don't close the connection
 });
 
 app.get('/hello', (req, res) => {
@@ -27,7 +26,45 @@ app.get('/hello', (req, res) => {
 });
 
 app.get('/posts', (req, res) => {
-  
-})
+  res.set('Content-Type', 'application/json');
+  conn.query('SELECT * FROM posts;', (err, rows) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(creatingJSON(rows));
+    }
+  });
+});
+
+app.post('/posts', (req, res) => {
+  let title = req.body.title;
+  let url = req.body.url;
+  conn.query(`INSERT INTO posts (title, url) VALUES (?, ?)`, [title, url], (err, rows) => {
+    if (err) {
+      res.send(err);
+    } else {
+      conn.query(`SELECT * FROM posts ORDER BY post_id DESC LIMIT 0, 1`, (err, rows) => {
+        if (err) {
+          res.send(err);
+        } else {
+          res.status(200).send(creatingJSON(rows));
+        }
+      });
+    }
+  });
+});
+
+function creatingJSON(rows) {
+  let returningJSON = { posts: [] };
+  rows.forEach(element => {
+    let objectToPush = {};
+    for (let i in element) {
+      objectToPush[i] = element[i];
+    }
+    returningJSON.posts.push(objectToPush)
+  });
+  console.log(returningJSON);
+  return returningJSON;
+}
 
 app.listen(PORT, () => `App listening on ${PORT}`);
