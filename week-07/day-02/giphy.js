7d20e46816cf837339a6fd6d4eb9b5acda73bcb3
Noemi Puskas <amifontos@gmail.com>
2020-02-13 20:07:33 +0100

    giphy works

--- week-07/day-02/giphy.js
+++ week-07/day-02/giphy.js
@@ -1,17 +1,26 @@
 'use strict';
 
-let APIKey = '0k0XK18UGfVyHx5qyfccT04bMSzxuB8M';
-let APIEndpoint = 'api.giphy.com/v1/gifs/search';
-let category = 'funny'
+const APIKey = '0k0XK18UGfVyHx5qyfccT04bMSzxuB8M';
+const APIEndpoint = 'https://api.giphy.com/v1/gifs/search';
+const category = 'funny'
+const pictureContainer = document.querySelector('main');
 
-let httpRequest = new XMLHttpRequest();
-let generatedRequestURL = APIEndpoint + '?api_key=' + APIKey + '&q=' + category;
+const xhr = new XMLHttpRequest();
+const generatedRequestURL = APIEndpoint + '?api_key=' + APIKey + '&q=' + category + '&limit=16&offset=0&rating=G&lang=en';
 console.log(generatedRequestURL);
-httpRequest.open('GET', generatedRequestURL, true);
-httpRequest.onreadystatechange = (event) => {
-  console.log(event.target.readyState);
+
+xhr.open('GET', generatedRequestURL, true);
+xhr.send();
+xhr.onreadystatechange = (event) => {
+  //console.log(event.target.readyState);
   if (event.target.readyState === 4 && event.target.status === 200) {
-    let incomingData = JSON.parse.response;
+    let incomingData = JSON.parse(xhr.response);
     console.log(incomingData);
+    for (let gif of incomingData.data){
+      let thumbnail = document.createElement('div');
+      thumbnail.innerHTML = `<a href ="${gif.url}" target="_blank"><img src = "${gif.images.fixed_width_small_still.url}" alt = "funny gif"></a> `
+      console.log(thumbnail.innerHTML);
+      pictureContainer.appendChild(thumbnail);
+    }
   }
 }
